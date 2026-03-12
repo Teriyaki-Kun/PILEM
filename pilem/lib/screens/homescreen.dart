33c3247f1f1255ca7c6b85425e43dc73fc357b5e
Michael Hermanto <michhu1212@gmail.com>
2026-03-12 16:00:07 +0700

    test

--- pilem/lib/screens/homescreen.dart
+++ pilem/lib/screens/homescreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pilem/models/movie.dart';
+import 'package:pilem/screens/detailscreen.dart';
 import 'package:pilem/services/apiservices.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,50 +11,61 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final Apiservices _apiservices = Apiservices();
+  final ApiService _apiService = ApiService();
 
   List<Movie> _allMovies = [];
   List<Movie> _trendingMovies = [];
   List<Movie> _popularMovies = [];
 
   Future<void> _loadMovies() async {
-    final List<Map<String, dynamic>> allMoviesData = await _apiservices.getAllMovies();
-    final List<Map<String, dynamic>> trendingMoviesData = await _apiservices.getTrendingMovie();
-    final List<Map<String, dynamic>> popularMoviesData = await _apiservices.getPopularMovies();
+    final List<Map<String, dynamic>> allMoviesData = await _apiService
+        .getAllMovies();
+    final List<Map<String, dynamic>> trendingMoviesData = await _apiService
+        .getTrendingMovies();
+    final List<Map<String, dynamic>> popularMoviesData = await _apiService
+        .getPopularMovies();
 
     setState(() {
       _allMovies = allMoviesData.map((e) => Movie.fromJson(e)).toList();
-      _trendingMovies = trendingMoviesData.map((e) => Movie.fromJson(e)).toList();
+      _trendingMovies = trendingMoviesData
+          .map((e) => Movie.fromJson(e))
+          .toList();
       _popularMovies = popularMoviesData.map((e) => Movie.fromJson(e)).toList();
     });
   }
 
-
-
+  @override
+  void initState() {
+    super.initState();
+    _loadMovies();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Pilem'),
-      ),
+      appBar: AppBar(title: const Text('Pilem')),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            //ListMovies
-            //Trending Movies
-            //Popular Movies
+            //List Movies
+            _buildMovieList('All Movies', _allMovies),
+            //list popular
+            _buildMovieList('Popular Movies', _popularMovies),
+            //List trending
+            _buildMovieList('Trending Movies', _trendingMovies),
           ],
         ),
       ),
     );
   }
+}
 
-  Widget _buildMovieList(String title, List<Movie> movies) {
+Widget _buildMovieList(String title, List<Movie> movies) {
   return Column(
     crossAxisAlignment: CrossAxisAlignment.start,
     children: [
+      // Menampilkan judul kategori
       Padding(
         padding: EdgeInsets.all(8.0),
         child: Text(
@@ -61,29 +73,39 @@ class _HomeScreenState extends State<HomeScreen> {
           style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
         ),
       ),
+      // Menampilkan thumbnail dan judul movie
       SizedBox(
         height: 200,
         child: ListView.builder(
           scrollDirection: Axis.horizontal,
           itemCount: movies.length,
-          itemBuilder: (BuildContext context, int index) {
+          itemBuilder: (BuildContext build, int index) {
             final Movie movie = movies[index];
-            return Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  Image.network(
-                    movie.imgUrl + movie.posterPath,
-                    height: 150,
-                    width: 100,
-                    fit: BoxFit.cover,
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    movie.title,
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ],
+            return GestureDetector(
+              onTap: () => Navigator.push(
+                Context,
+                MaterialPageRoute(
+                  builder: (context) => DetailScreen(movie: movie),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Image.network(
+                      'https://image.tmdb.org/t/p/w500${movie.posterPath}',
+                      height: 150,
+                      width: 100,
+                      fit: BoxFit.cover,
+                    ),
+                    Text(
+                      movie.title.length > 14
+                          ? '${movie.title.substring(0, 10)}...'
+                          : movie.title,
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
               ),
             );
           },
@@ -91,5 +113,4 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     ],
   );
-  }
 }
